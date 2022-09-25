12ed3f030fb06961ed46211bf6e4d8ea9d45f6f4
P1cks <dmitry.udoven4ik@yandex.ru>
2022-09-25 15:42:46 +0300

    Добавлена система регистрации, логина и логаута

--- wsgi.py
+++ wsgi.py
@@ -1,17 +1,82 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Length, AnyOf
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '4ebbbfe7706b4077a6c7fed83e59f0dd'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
+
+class User(UserMixin, db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(15), unique=True)
+	password = db.Column(db.String(80))
+
+@login_manager.user_loader
+def load_user(user_id):
+	return User.query.get(int(user_id))
+
+class LoginForm(FlaskForm):
+	username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+	password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+	remember = BooleanField('remember me')
+
+class RegisterForm(FlaskForm):
+	username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+	password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
 
 @app.route('/')
 def index():
-	return render_template('hello.html')
+	return render_template('index.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+
+	if form.validate_on_submit():
+		user = User.query.filter_by(username=form.username.data).first()
+		if user:
+			if check_password_hash(user.password, form.password.data):
+				login_user(user, remember=form.remember.data)
+				return redirect('/1')
+
+		return '<h1>Invalid username or password.</h1>'
+
+	return render_template('LoginForm.html', title='Login', form=form)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+	form = RegisterForm()
+
+	if form.validate_on_submit():
+		hashed_password = generate_password_hash(form.password.data, method='sha256')
+		new_user = User(username=form.username.data, password=hashed_password)
+		db.session.add(new_user)
+		db.session.commit()
+
+		return '<h1>New user has been created!</h1>'
+
+	return render_template('RegisterForm.html', title='Sign Up', form=form)
 
 @app.route('/<int:quest>')
+@login_required
 def asking(quest):
 	return render_template('question.html', title='Questions', quest=quest)
+
+@app.route('/logout')
+def logout():
+	logout_user()
+	return redirect('/')
 
 
 if __name__ == '__main__':
